7781af9e9dafb174a2009ac4cceafaff24efd2d2
raunak-rajj <raunakrajarya1@gmail.com>
2024-10-21 11:19:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 98399
+let accountEmail = "raunakrajarya1@gmail.com"
+var accountPassword = "112345"
+accountCity = "hyderabad"
+let accountState;
+
+//accountId = 2568
+accountEmail = "raunakraj@gmail.com"
+accountPassword = "112345678"
+accountCity = "benguluru"
+
+//console.log({accountId,accountEmail,accountPassword,accountCity});
+
+console.table({accountId,accountEmail,accountPassword,accountCity,accountState});
